03ce4cd109728f3c455ec8f71456e38cb3d1fcc1
Tom <tom.rosenstrom@iki.fi>
2011-09-03 00:13:52 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Muky::Application.config.secret_token = '331be5cdc40ac381527f312d419aafe5bcc45c6cbcf8e398f60895501374f04f6acc692120c9affb590577e430fc80e2ca1a89a6233a3b6418aacbff06be2638'
